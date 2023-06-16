0f3c1c187ab745bf8044e21022d9004f869ccafe
fiatrete <fiatrete@gmail.com>
2023-06-16 15:58:57 +0800

    'do_nothing' is no longer useful

--- agent_jarvis/jarvis/functional_modules/communication.py
+++
@@ -1,9 +0,0 @@
-from jarvis.functional_modules.functional_module import functional_module, CallerContext
-
-
-@functional_module(
-    name="do_nothing",
-    description="Do nothing. This is not an ability, just a way to let you refuse",
-    signature={})
-async def do_nothing(context: CallerContext):
-    return "Success"
