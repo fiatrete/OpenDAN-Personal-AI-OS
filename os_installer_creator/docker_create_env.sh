c7d3c047fde48235069ce34e492f5514523fb477
fiatrete <fiatrete@gmail.com>
2023-06-20 10:34:27 +0800

    Temporarily remove the os_installer directory to prevent misunderstandings.

--- os_installer_creator/docker_create_env.sh
+++
@@ -1,9 +0,0 @@
-CONTAINER_NAME=opendan-installer-container
-docker run -d -it --name ${CONTAINER_NAME} \
---platform=linux/amd64 \
---security-opt apparmor:unconfined --cap-add SYS_ADMIN \
-ubuntu:jammy /bin/bash
-
-docker cp ./scripts ${CONTAINER_NAME}:/root/
-docker exec -it ${CONTAINER_NAME} sh -c 'apt update && apt install -y lsb-release sudo'
-docker exec -it ${CONTAINER_NAME} /root/scripts/build.sh setup_host 
