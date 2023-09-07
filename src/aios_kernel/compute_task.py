3eac598c972c7a8c3a1ecc01cae76202118f898d
Song <glen0125@foxmail.com>
2023-09-07 12:50:13 +0800

    Add stability node (Text2Img)

--- src/aios_kernel/compute_task.py
+++ src/aios_kernel/compute_task.py
@@ -3,6 +3,7 @@ from enum import Enum
 import uuid
 import time
 
+
 class ComputeTaskState(Enum):
     DONE = 0
     INIT = 1
@@ -11,22 +12,31 @@ class ComputeTaskState(Enum):
     PENDING = 4
 
 
+class ComputeTaskType(Enum):
+    NONE = -1
+    LLM_COMPLETION = 0
+    TEXT_2_IMAGE = 1
+    IMAGE_2_IMAGE = 2
+    VOICE_2_TEXT = 3
+    TEXT_2_VOICE = 4
+
+
 class ComputeTask:
     def __init__(self) -> None:
         self.task_type = "llm_completion"
         self.create_time = None
 
-        self.task_id:str = None
-        self.callchain_id:str = None
-        self.params:dict = {}
-        self.refers:dict = None
-        self.pading_data:bytearray = None
+        self.task_id: str = None
+        self.callchain_id: str = None
+        self.params: dict = {}
+        self.refers: dict = None
+        self.pading_data: bytearray = None
 
         self.state = ComputeTaskState.INIT
         self.result = None
         self.error_str = None
 
-    def set_llm_params(self,prompts,model_name,max_token_size,callchain_id = None):
+    def set_llm_params(self, prompts, model_name, max_token_size, callchain_id=None):
         self.task_type = "llm_completion"
         self.create_time = time.time()
         self.task_id = uuid.uuid4().hex
@@ -34,7 +44,7 @@ class ComputeTask:
         self.params["prompts"] = prompts.messages
         if model_name is not None:
             self.params["model_name"] = model_name
-        else: 
+        else:
             self.params["model_name"] = "gpt-4-0613"
         self.params["max_token_size"] = max_token_size
 
@@ -45,16 +55,16 @@ class ComputeTask:
 class ComputeTaskResult:
     def __init__(self) -> None:
         self.create_time = None
-        self.task_id:str = None    
-        self.callchain_id:str = None 
-        self.worker_id:str = None
-        self.result_code:int = 0
-        self.result_str:str = None   
+        self.task_id: str = None
+        self.callchain_id: str = None
+        self.worker_id: str = None
+        self.result_code: int = 0
+        self.result_str: str = None
 
-        self.result:dict = {}
-        self.result_refers:dict = None
-        self.pading_data:bytearray = None
+        self.result: dict = {}
+        self.result_refers: dict = None
+        self.pading_data: bytearray = None
 
-    def set_from_task(self,task:ComputeTask):
+    def set_from_task(self, task: ComputeTask):
         self.task_id = task.task_id
         self.callchain_id = task.callchain_id
