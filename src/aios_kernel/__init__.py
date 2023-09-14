cbfff71d6bffc663f61d626879704066d358c7b6
wugren <wuguoren@buckyos.com>
2023-09-15 00:33:18 +0800

    add whisper speech to text node and google text to speech node

--- src/aios_kernel/__init__.py
+++ src/aios_kernel/__init__.py
@@ -4,8 +4,10 @@ from .chatsession import AIChatSession
 from .agent import AIAgent,AIAgentTemplete,AgentPrompt
 from .compute_kernel import ComputeKernel,ComputeTask
 from .compute_node import ComputeNode,LocalComputeNode
-from .open_ai_node import OpenAI_ComputeNode 
+from .open_ai_node import OpenAI_ComputeNode
 from .role import AIRole,AIRoleGroup
 from .workflow import Workflow
 from .bus import AIBus
 from .workflow_env import WorkflowEnvironment,CalenderEnvironment,CalenderEvent
+from .whisper_node import WhisperComputeNode
+from .google_text_to_speech_node import GoogleTextToSpeechNode
